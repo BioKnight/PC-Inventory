0b983e675f051d835c7c17bd047346f81eadbd1b
Randy <randycmiller1981@gmail.com>
2016-07-03 15:47:25 -0400

    Added two Classes one for Importing Belarc profiles, and one for creating new profiles of the current machine.

---
+++ PC Inventory Acton/Create_PC_Profile.vb
@@ -0,0 +1,11 @@
+﻿''' <summary>
+''' Creates a new profile of the current PC and adds it to the DB.
+''' </summary>
+Public Class Create_PC_Profile
+    ''' <summary>
+    ''' Initialize a new Blank instance of this class.
+    ''' </summary>
+    Sub New()
+
+    End Sub
+End Class
---
+++ PC Inventory Acton/Read_Belarc_Profile.vb
@@ -0,0 +1,12 @@
+﻿''' <summary>
+''' Reads information from HTML documents created from the Belarc Advisor.
+''' </summary>
+Public Class Read_Belarc_Profile
+    ''' <summary>
+    ''' Initialize a new Blank instance of this class.
+    ''' </summary>
+    Sub New()
+
+    End Sub
+
+End Class
