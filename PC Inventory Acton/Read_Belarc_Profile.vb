11d289867226e1c7266726a3e058f0c38132b144
Randy <randycmiller1981@gmail.com>
2016-07-06 08:01:38 -0400

    Added a couple more functions to read the HTML files from Belarc

--- PC Inventory Acton/Read_Belarc_Profile.vb
+++ PC Inventory Acton/Read_Belarc_Profile.vb
@@ -1,12 +1,183 @@
-﻿''' <summary>
+﻿Imports System.IO
+
+
+
+''' <summary>
 ''' Reads information from HTML documents created from the Belarc Advisor.
 ''' </summary>
 Public Class Read_Belarc_Profile
+
+    Private html_list As New List(Of HTML_Document_Array)
+    Private File_List As New List(Of FileInfo)
+
     ''' <summary>
     ''' Initialize a new Blank instance of this class.
     ''' </summary>
     Sub New()
 
     End Sub
+    ''' <summary>
+    ''' Reads an HTML file into an array for parsing.
+    ''' </summary>
+    ''' <param name="file">A file as a string. (ex: C:\profiles\profile.html)</param>
+    Public Sub read_html_Profile(file As String)
+
+    End Sub
+    ''' <summary>
+    ''' Reads an HTML file into an array for parsing.
+    ''' </summary>
+    ''' <param name="file">A file as a Fileinfo Object.</param>
+    Public Sub read_html_Profile(file As FileInfo)
+
+    End Sub
+    ''' <summary>
+    ''' Loads a directory and reads all HTML files into seperate arrays for parsing.
+    ''' </summary>
+    ''' <param name="directory">A directory as a string. (ex: C:\profiles)</param>
+    Public Sub load_Directory(directory As String)
+
+    End Sub
+    ''' <summary>
+    ''' Loads a directory and reads all HTML files into seperate arrays for parsing.
+    ''' </summary>
+    ''' <param name="directory">A directory as a DirectoryInfo object.</param>
+    Public Sub load_Directory(directory As DirectoryInfo)
+        For Each file As FileInfo In directory.EnumerateFiles
+            If LCase(file.Extension) = ".html" Then
+                File_List.Add(file)
+                html_list.Add(New HTML_Document_Array(file))
+            End If
+        Next
+    End Sub
+
+    Public Function get_element() As String
+        Return html_list(0).get_Element()
+    End Function
+
+    Public Function get_all_Elements()
+        'Return html_list(0).get_all_Elements
+    End Function
+
+    Public ReadOnly Property open_Files As Integer
+        Get
+            Return html_list.Count
+        End Get
+    End Property
+
+    Public ReadOnly Property Name(Optional index As Integer = 0) As String
+        Get
+            Return html_list(index).pc_name
+        End Get
+    End Property
+
+    Public ReadOnly Property Profile_Date(Optional index As Integer = 0) As String
+        Get
+            Return html_list(index).profile_date
+        End Get
+    End Property
+
+    Public ReadOnly Property file(index As Integer) As FileInfo
+        Get
+            Return File_List(index)
+        End Get
+    End Property
+
+    Public ReadOnly Property Current_Login(index As Integer) As String
+        Get
+            Return html_list(index).Current_Login
+        End Get
+    End Property
+
+    Public ReadOnly Property os(index As Integer) As String
+        Get
+            Return html_list(index).os
+        End Get
+    End Property
+End Class
+
+Public Class HTML_Document_Array
+    Private doc As String
+
+    Private comparisons() As StringComparison = CType([Enum].GetValues(GetType(StringComparison)), StringComparison())
+
+
+    Public ReadOnly Property html_Document
+        Get
+            Return doc
+        End Get
+    End Property
+
+    Sub New(html_Doc As FileInfo)
+
+        Using reader As StreamReader = New StreamReader(html_Doc.FullName)
+            doc = reader.ReadToEnd
+        End Using
+
+    End Sub
+
+    Public Function get_Element() As String
+        Return doc
+    End Function
+
+    Public Function pc_name() As String
+
+        Try
+            Dim s As String = doc.Substring(doc.IndexOf("<body>", comparisons(5)))
+            s = s.Substring(s.IndexOf("Computer Name:", comparisons(5)))
+            s = s.Substring(s.IndexOf("<TD>", comparisons(5)) + 4)
+            s = s.Substring(0, s.IndexOf("<", comparisons(5)))
+            s = s.Substring(0, s.IndexOf("(", comparisons(5)))      ' This could cause an issue if the ( is missing.
+            s = s.Replace(vbCr, "").Replace(vbLf, "").Trim
+            Return s
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    Public Function Current_Login() As String
+        'Profile Date:
+        Try
+            Dim s As String = doc.Substring(doc.IndexOf("<body>", comparisons(5)))
+            s = s.Substring(s.IndexOf("Windows Logon:", comparisons(5)))
+            s = s.Substring(s.IndexOf("<TD>", comparisons(5)) + 4)
+            s = s.Substring(0, s.IndexOf("<", comparisons(5)))
+            s = s.Replace(vbCr, "").Replace(vbLf, "").Trim
+            Return s
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    Public Function os() As String
+        'Profile Date:
+        Try
+            Dim s As String = doc.Substring(doc.IndexOf("<body>", comparisons(5)))
+            s = s.Substring(s.IndexOf("Operating System", comparisons(5)))
+            s = s.Substring(s.IndexOf("<TD>", comparisons(5)) + 4)
+            s = s.Substring(0, s.IndexOf("<", comparisons(5)))
+            s = s.Replace(vbCr, "").Replace(vbLf, "").Trim
+            s = s.Replace("   ", " ").Replace("   ", " ").Trim
+            s = s.Replace("  ", " ").Replace("  ", " ").Trim
+            Return s
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    Public Function profile_date() As String
+        'Profile Date:
+        Try
+            Dim s As String = doc.Substring(doc.IndexOf("<body>", comparisons(5)))
+            s = s.Substring(s.IndexOf("Profile Date:", comparisons(5)))
+            s = s.Substring(s.IndexOf("<TD>", comparisons(5)) + 4)
+            s = s.Substring(0, s.IndexOf("<", comparisons(5)))
+            s = s.Replace(vbCr, "").Replace(vbLf, "").Trim
+            Return s
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+
 
 End Class
--- PC Inventory Acton/frm_Main.vb
+++ PC Inventory Acton/frm_Main.vb
@@ -1,3 +1,43 @@
-﻿Public Class frm_Main
+﻿Imports System.IO
+Public Class frm_Main
+    Private index_temp As Integer = 0
+    Private belarc_read As New Read_Belarc_Profile
+    Private Sub frm_Main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        belarc_read.load_Directory(New DirectoryInfo("C:\Users\Randy\Desktop\Computer Profiles"))
+        lbl_Record_Count.Text = belarc_read.open_Files
+        fill_form()
+    End Sub
+
+    Private Sub btn_Last_Click(sender As Object, e As EventArgs) Handles btn_Last.Click
+        index_temp = belarc_read.open_Files - 1
+        fill_form()
+    End Sub
+
+    Private Sub btn_First_Click(sender As Object, e As EventArgs) Handles btn_First.Click
+        index_temp = 0
+        fill_form()
+    End Sub
+
+    Private Sub btn_Next_Click(sender As Object, e As EventArgs) Handles btn_Next.Click
+        If index_temp >= belarc_read.open_Files - 1 Then Exit Sub
+        index_temp += 1
+        fill_form()
+    End Sub
+
+    Private Sub btn_Previous_Click(sender As Object, e As EventArgs) Handles btn_Previous.Click
+        If index_temp <= 0 Then Exit Sub
+        index_temp -= 1
+        fill_form()
+    End Sub
+
+    Sub fill_form()
+        lbl_cur_Record.Text = index_temp + 1
+        lbl_PC_Name.Text = belarc_read.Name(index_temp)
+        lbl_Updated.Text = belarc_read.Profile_Date(index_temp)
+        lbl_File_Location.Text = belarc_read.file(index_temp).FullName
+        lbl_User.Text = belarc_read.Current_Login(index_temp)
+        lbl_OS.Text = belarc_read.os(index_temp)
+    End Sub
+
 
 End Class
